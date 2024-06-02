a623b9e399ba52f245680d249b8b9a1e6d1cf7e8
Aryan10 <aryan.shivam.10@gmail.com>
2024-06-02 09:25:28 +0530

    Updated site

---
+++ src/AppRouter.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './components/Home';
+import Projects from './components/Projects';
+import Navbar from './components/Navbar';
+
+const AppRouter = () => {
+  return (
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/projects" element={<Projects />} />
+      </Routes>
+    </Router>
+  );
+};
+
+export default AppRouter;
