0883edc93dcca4839b17b9434b8b80b45d5681f1
Aryan10 <aryan.shivam.10@gmail.com>
2024-07-03 17:36:37 +0530

    Updated time

--- src/components/data/projects.js
+++ src/components/data/projects.js
@@ -6,28 +6,28 @@ const projectList = [
         repoURL: github + "Aryan10/Aryan10.github.io",
         description: "Simple portfolio website, in development",
         techstack: ["reactjs", "css", "git", "github-pages"],
-        timeperiod: "June 2024",
+        timeperiod: "2024",
     },
     {
         title: "Website: E-Commerce Platform",
         repoURL: github + "Nishant040305/Vend-Ventures",
         description: "Online marketplace where users can both buy and sell. Includes user login, chatting, cart and wishlist. Made in one week for Weekend of Code 2024 @ MNNIT (Top 10 Teams)",
         techstack: ["mongodb", "nodejs", "expressjs", "reactjs", "socketio", "googleauth", "git"],
-        timeperiod: "April 2024",
+        timeperiod: "2024",
     },
     {
         title: 'Website: Currency Converter',
         repoURL: github + "Aryan10/SimpleCurrencyConverter",
         description: "My first website. A minimalist currency exchange converter with trends, graphs, history and favourites. Made in one week for CodeSangam2023 @ MNNIT",
         techstack: ["javascript", "html", "css", "fetch", "chartjs"],
-        timeperiod: "September 2023",
+        timeperiod: "2023",
     },
     {
         title: 'Discord Bot (Z-Bot)',
         repoURL: github + "Aryan10/Z-Bot",
         description: 'Made several discord bots for moderation, utility, currency, fun and informational commands. The open source Z-bot (4000 servers when hosted) provides detailed Pokemon information.',
         techstack: ["nodejs", "discord-api", "mongodb", "request"],
-        timeperiod: "July 2023",
+        timeperiod: "2023",
     },
 ];
 
